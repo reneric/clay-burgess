7a2df0b539ad4eb75c0da489b9fcb0630683702f
Ren Simmons <rsimmo4@gmail.com>
2013-10-25 13:21:16 -0600

    First commit

--- footer.php
+++ footer.php
@@ -18,6 +18,7 @@
 				<?php do_action( 'twentythirteen_credits' ); ?>
 				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentythirteen' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentythirteen' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentythirteen' ), 'WordPress' ); ?></a>
 			</div><!-- .site-info -->
+			<script type="text/javascript" data-project="clay-burgess" src="http://localhost:48626/takana.js"></script>
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
 
